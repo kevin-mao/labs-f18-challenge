0f83b00803ac2a0ef080ab0b5a7f64905dfe09c8
Kevin Mao <km3290@columbia.edu>
2018-09-01 13:06:28 -0400

    added pokemon api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +7,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+
+@app.route('/pokemon/<query>', methods=['GET'])
+def get_pokemon(query):	
+	info = requests.get('https://pokeapi.co/api/v2/pokemon/' + query).json()	
+	pokemon = info['forms'][0]['name']
+	return render_template('pokemon.html', query=query, pokemon=pokemon)
+
 
 
 if __name__ == '__main__':
