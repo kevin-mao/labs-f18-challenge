9a25a30ba37d24192d123d1235c2d6b4bc9ce452
Kevin Mao <km3290@columbia.edu>
2018-09-07 21:38:25 -0400

    added feature: can input id or name in query

--- app.py
+++ app.py
@@ -11,9 +11,21 @@ def main():
 @app.route('/pokemon/<query>', methods=['GET'])
 def get_pokemon(query):	
 	info = requests.get('https://pokeapi.co/api/v2/pokemon/' + query).json()	
-	pokemon = info['forms'][0]['name']
-	return render_template('pokemon.html', query=query, pokemon=pokemon)
+	#check if the input is an int
+	try: 
+		#if this works then we already have id
+		id = int(query)
+		name = info['name'].capitalize()
+		order = 'id'
 
+	#if it is an exception, query is the pokemon name 
+	except ValueError:
+		#so extract id
+		id = info['id']
+		name = query.capitalize()
+		order = 'name'
+	
+	return render_template('pokemon.html', id=id, name=name, order=order)
 
 
 if __name__ == '__main__':
